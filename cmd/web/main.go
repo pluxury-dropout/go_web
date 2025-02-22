b14ec86b39faf087bfda01aef2c127ae1d6689c8
pluxury_dropout <104727377+pluxury-dropout@users.noreply.github.com>
2025-02-22 06:05:34 +0500

    from september 2024

---
+++ cmd/web/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"go_web/internal/models"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/go-playground/form"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type application struct {
+	errorLog       *log.Logger
+	infoLog        *log.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
+	sessionManager *scs.SessionManager
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:123123@/snippetbox?parseTime=true", "MySQL data source name")
+
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	formDecoder := form.NewDecoder()
+
+	sessionMaganer := scs.New()
+	sessionMaganer.Store = mysqlstore.New(db)
+	sessionMaganer.Lifetime = 12 * time.Hour
+
+	app := &application{
+		errorLog:       errorLog,
+		infoLog:        infoLog,
+		snippets:       &models.SnippetModel{DB: db},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionMaganer,
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module go_web
+
+go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/justinas/alice v1.2.0
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/alexedwards/scs v1.4.1 // indirect
+	github.com/alexedwards/scs/mysqlstore v0.0.0-20240316134038-7e11d57e8885 // indirect
+	github.com/alexedwards/scs/v2 v2.8.0 // indirect
+	github.com/go-playground/form v3.1.4+incompatible // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
